f9a4fc242dd131e271ae56b17a0d84e9d916dd6d
SuchithSridhar <suchithsridhar@gmail.com>
2020-03-20 14:38:38 +0300

    Started Sort Function

--- Main.java
+++ Main.java
@@ -1,18 +1,53 @@
+import java.util.Arrays;
+
 class Main{
     public static void main(String[] args){
         System.out.println("Program Begins");
-        createRoom();
+        sortingDemo();
     }
 
     static void createRoom(){
+        System.out.println("-------------------- Demo of Classes and Inheritance --------------------");
         Room my_room = new Room(12,14,4);
-        System.out.println("The area of the room : "+ my_room.area());
-        System.out.println("The volume of the room : "+my_room.volume());
-        System.out.println("The height of the room : " + my_room.height);
 
+        printAllTheseStrings(
+            ("The area of the room : "+ my_room.area()),
+            ("The volume of the room : "+my_room.volume()),
+            ("The height of the room : " + my_room.height)
+        );
+        // variable number of arguments
+
+        
         System.out.println();
 
         Bedroom newroom = new Bedroom(10, 10, 4, 22);
         newroom.specifications();
+        System.out.println("--------------------------------------------------------------------------");
+    }
+
+    static void abstractions(){
+        System.out.println("--------------------"
+        + " Demo of Abstract and Multiple inheritance "
+        + "--------------------");
+
+    }
+
+    static void printAllTheseStrings(String...var_name){
+        for(String item:var_name){
+            System.out.println(item);
+        }
+    }
+
+    static void sortingDemo(){
+        int[] example = new int[5];
+        int[] array = {12,16,2,8,11};
+
+        example[1] = 1;
+        
+        System.out.println("--------------------"
+        + " Sorting Demo "
+        + "--------------------");
+
+        System.out.println(Arrays.toString(array));
     }
 }
